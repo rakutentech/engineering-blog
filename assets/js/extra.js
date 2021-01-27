7126534a50251559695b87962162f2f0a1a2638d
Boaz Yaniv <boaz.yaniv@rakuten.com>
2021-01-27 12:41:43 +0900

    fix: Make dark mode detection on local storage
    Dark mode detection should not rely on inline CSS style values.

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -1,27 +1,24 @@
 (function() {
-  // Add observer to change the color of the logo in dark mode
-  // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-  const targetNode = document.body;
-  const config = {
-    attributes: true,
-    childList: false,
-    subtree: false
-  };
-  const callback = function(mutationsList, observer) {
-    if (targetNode.getAttribute("style") === "--tooltip:lightgrey;") {
-      // Light mode
-      if (targetNode.classList.contains('dark')) {
-        targetNode.classList.remove("dark");
-      } else {
-      }
-    } else {
-      // Dark mode
-      if (targetNode.classList.contains('dark')) {
-      } else {
-        targetNode.classList.add("dark");
-      }
+  function onAttributeChange(targetNode, callback) {
+    // Add observer to change the color of the logo in dark mode
+    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+    const config = {
+      attributes: true,
+      childList: false,
+      subtree: false
     }
-  };
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
+    const observer = new MutationObserver(callback)
+    observer.observe(targetNode, config)
+  }
+
+  const targetNode = document.body
+  onAttributeChange(targetNode, () => {
+    const newModeIsDark = (localStorage.getItem('isLight') === 'false')
+    const oldModeIsDark = targetNode.classList.contains('dark')
+    if (newModeIsDark && !oldModeIsDark) {
+        targetNode.classList.add('dark')
+    } else if (!newModeIsDark && oldModeIsDark) {
+        targetNode.classList.remove('dark')
+    }
+  })
 })()
